3ccd21be1dba8fd643a325eb4204c7dca7b0a110
Nathan Wilson <nathan@collectivesource.com>
2014-01-24 06:36:33 -0500

    Commented out some internal specs to check for dead code

--- spec/taxon_reporter/eol_data_source_spec.rb
+++ spec/taxon_reporter/eol_data_source_spec.rb
@@ -1,43 +1,45 @@
 require_relative '../spec_helper'
 
 describe TaxonReporter::EolDataSource do
-  it ".data with unknown id" do
-    stub_request(:get, TaxonReporter::EolDataSource.pages_url(0)).to_return(File.new 'fixtures/webmock/p_0')
-    expect(TaxonReporter::EolDataSource.data(0)).to eq({})
-  end
+  pending "need to write some new tests"
   
-  it ".filter_hes" do
-    expect(TaxonReporter::EolDataSource.filter_hes(Set.new([[1, "ITIS"], [2, "GBIF Nub Taxonomy"], [3, "Index Fungorum"]]))).to eq(Set.new([1,3]))
-  end
- 
-  context "Giraffa" do
-    let(:family) { 'Giraffa' }
-    let(:id) { 38813 }
-  
-    it ".get_eol_ids_from_name" do
-      stub_request(:get, TaxonReporter::EolDataSource.search_url(family)).to_return(File.new 'fixtures/webmock/s_Giraffa')
-      expect(TaxonReporter::EolDataSource.get_eol_ids_from_name(family)).to eq(Set.new([id]))
-    end
-    
-    it ".data" do
-      stub_request(:get, TaxonReporter::EolDataSource.pages_url(38813)).to_return(File.new 'fixtures/webmock/p_38813')
-      [52578134, 49481246, 46598317, 49222053, 49339384, 50650187, 51377284, 24935172, 54413991, 55943656].each do |id|
-        stub_request(:get, TaxonReporter::EolDataSource.hierarchy_entries_url(id)).to_return(File.new "fixtures/webmock/he_#{id}")
-      end
-      expect(TaxonReporter::EolDataSource.data(id)["scientificName"]).to eq(Set.new([family]))
-    end
-    
-    it ".search_url" do
-      expect(TaxonReporter::EolDataSource.search_url(family)).to match(/api\/search\/#{family}.json/)
-    end
-  
-    it ".pages_url" do
-      expect(TaxonReporter::EolDataSource.pages_url(id)).to match(/api\/pages\/#{id}.json/)
-    end
-
-    it ".hierarchy_entries_url" do
-      expect(TaxonReporter::EolDataSource.hierarchy_entries_url(id)).to match(/api\/hierarchy_entries\/#{id}.json/)
-    end
-  
-  end
+  # it ".data with unknown id" do
+  #   stub_request(:get, TaxonReporter::EolDataSource.pages_url(0)).to_return(File.new 'fixtures/webmock/p_0')
+  #   expect(TaxonReporter::EolDataSource.data(0)).to eq({})
+  # end
+  # 
+  # it ".filter_hes" do
+  #   expect(TaxonReporter::EolDataSource.filter_hes(Set.new([[1, "ITIS"], [2, "GBIF Nub Taxonomy"], [3, "Index Fungorum"]]))).to eq(Set.new([1,3]))
+  # end
+  #  
+  # context "Giraffa" do
+  #   let(:family) { 'Giraffa' }
+  #   let(:id) { 38813 }
+  # 
+  #   it ".get_eol_ids_from_name" do
+  #     stub_request(:get, TaxonReporter::EolDataSource.search_url(family)).to_return(File.new 'fixtures/webmock/s_Giraffa')
+  #     expect(TaxonReporter::EolDataSource.get_eol_ids_from_name(family)).to eq(Set.new([id]))
+  #   end
+  #   
+  #   it ".data" do
+  #     stub_request(:get, TaxonReporter::EolDataSource.pages_url(38813)).to_return(File.new 'fixtures/webmock/p_38813')
+  #     [52578134, 49481246, 46598317, 49222053, 49339384, 50650187, 51377284, 24935172, 54413991, 55943656].each do |id|
+  #       stub_request(:get, TaxonReporter::EolDataSource.hierarchy_entries_url(id)).to_return(File.new "fixtures/webmock/he_#{id}")
+  #     end
+  #     expect(TaxonReporter::EolDataSource.data(id)["scientificName"]).to eq(Set.new([family]))
+  #   end
+  #   
+  #   it ".search_url" do
+  #     expect(TaxonReporter::EolDataSource.search_url(family)).to match(/api\/search\/#{family}.json/)
+  #   end
+  # 
+  #   it ".pages_url" do
+  #     expect(TaxonReporter::EolDataSource.pages_url(id)).to match(/api\/pages\/#{id}.json/)
+  #   end
+  # 
+  #   it ".hierarchy_entries_url" do
+  #     expect(TaxonReporter::EolDataSource.hierarchy_entries_url(id)).to match(/api\/hierarchy_entries\/#{id}.json/)
+  #   end
+  # 
+  # end
 end
--- spec/taxon_reporter/field_spec.rb
+++ spec/taxon_reporter/field_spec.rb
@@ -1,9 +1,5 @@
 require_relative '../spec_helper'
 
 describe TaxonReporter::Field do
-  let(:source) { 'Test' }
-  let(:name) { 'Name' }
-  subject(:field) { TaxonReporter::Field.new(source, name) }
-  
   pending "need to write some tests"
 end
